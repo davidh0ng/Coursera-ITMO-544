f9942d4014dc4f9c6c52bf8cce59f04246601bab
davidh0ng <djhong83@gmail.com>
2025-09-29 19:09:05 -0700

    updated lines 222 and 223 following example usage for autoscaling attachment

--- Module-07/main.tf
+++ Module-07/main.tf
@@ -219,8 +219,8 @@ resource "aws_autoscaling_group" "bar" {
 resource "aws_autoscaling_attachment" "example" {
   # Wait for lb to be running before attaching to asg
   depends_on  = [aws_lb.lb]
-  autoscaling_group_name = var.asg-name
-  lb_target_group_arn    = var.tg-name
+  autoscaling_group_name = aws_autoscaling_group.example.id
+  lb_target_group_arn    = aws_lb_target_group.example.arn
 }
 
 output "alb-lb-tg-arn" {
