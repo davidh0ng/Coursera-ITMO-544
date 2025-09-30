16efaddfe6e0f25ebb4634b91b7ec91adb270ce3
davidh0ng <djhong83@gmail.com>
2025-09-29 18:32:07 -0700

    updated line 110 to a string response in main.tf

--- Module-07/main.tf
+++ Module-07/main.tf
@@ -107,7 +107,7 @@ resource "aws_lb_target_group" "alb-lb-tg" {
   # resource is ready
   depends_on  = [aws_lb.lb]
   name        = var.tg-name
-  target_type = instance
+  target_type = "instance"
   port        = 80
   protocol    = "HTTP"
   vpc_id      = data.aws_vpc.main.id
