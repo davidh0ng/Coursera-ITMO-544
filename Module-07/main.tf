55443747c7807e507e398af4cd8db58c3ee70e8f
davidh0ng <djhong83@gmail.com>
2025-09-29 18:31:24 -0700

    updated vpc_id on line 113 in main.tf

--- Module-07/main.tf
+++ Module-07/main.tf
@@ -110,7 +110,7 @@ resource "aws_lb_target_group" "alb-lb-tg" {
   target_type = instance
   port        = 80
   protocol    = "HTTP"
-  vpc_id      = aws_vpc.main.id
+  vpc_id      = data.aws_vpc.main.id
 }
 
 ##############################################################################
